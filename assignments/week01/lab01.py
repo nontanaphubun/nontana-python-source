4f85f2496dde5fbb4c22062aa8119be87442cddc
nontanaphubun <nontana.p@ku.th>
2025-07-25 16:28:43 +0700

    ส่งงาน week01

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,18 @@
+# Exercise 1: personal Profile Creator
+print("=== Exercise 1: Basic Input ===")
+user_name = input("What is your name? ")
+print("Hello,", user_name, "!")
+print("Nice to meet you,", user_name)
+
+#write your solution here:
+full_name = input("What is your name?:")
+age= input("How old are you?")
+email = input("What is your email address?:")
+phone_number = input("What is your phone number?:")
+hobby =input ("What is your favorite hobby? :")
+print("===Your profile===")
+print("Full name:",full_name)
+print("age:",age)
+print("Email:",email)
+print("phone number:",phone_number)
+print("favorite hobby? :",hobby)
