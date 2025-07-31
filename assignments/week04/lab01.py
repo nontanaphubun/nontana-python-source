01d114def910eb833f184a3e4289212a9cdbcc50
nontanaphubun <nontana.p@ku.th>
2025-07-31 21:12:12 +0700

    .

--- assignments/week04/lab01.py
+++ assignments/week04/lab01.py
@@ -19,29 +19,34 @@ def personal_info_manager():
     person = ("Nontana Phubun", 20, "Chonburi", "Thailand")  # name, age, city, country
     hobbies = []
 
-    #your code here
-    print("1 Display into, 2 Add hobby, 3 Remove hobby, 4 Edit age, 5 Exit")
-    choice = input("What do you want to do?: ")
+    # your code here
+    while True:
+            
+            print("1 Display into, 2 Add hobby, 3 Remove hobby, 4 Edit age, 5 Exit")
+            choice = input("What do you want to do?: ")
 
-    if choice == "1":
-            print("Name: ", preson[0])
-            print("Age: ", preson[1])
-            print("City: ", preson[2])
-            print("Country: ", preson[3])
-            print("Hobbies: ", hobbies)
+        if choice == "1":
+                print("Name: ", preson[0])
+                print("Age: ", preson[1])
+                print("City: ", preson[2])
+                print("Country: ", preson[3])
+                print("Hobbies: ", hobbies)
 
         elif choice == "2":
-            hobby = input("Insert new hobby: ")
-            hobbies.append(hobby)
+                hobby = input("Insert new hobby: ")
+                hobbies.append(hobby)
 
         elif choice == "3":
-            del hobbies[0]
+                del hobbies[0]
 
         elif choice == "4":
-            age = input("Insert new age: ")
-            person_list = list(person) #["Nontana Phubun", 20, "Chonburi", "Thailand"]
-            person_list[1] = age
-            person = tuple(person_list)
+                age = input("Insert new age: ")
+                person_list = list(person) #["Nontana Phubun", 20, "Chonburi", "Thailand"]
+                person_list[1] = age
+                person = tuple(person_list)
+
+        elif choice == "5":
+            brek
 
 if __name__ == "__main__":
     personal_info_manager()
